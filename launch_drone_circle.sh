9a2b1516c3c19e351b8a2281bec61674eaad10a1
Olipet24 <123438907+Olipet24@users.noreply.github.com>
2025-06-02 14:54:45 -0700

    Add files via upload

---
+++ launch_drone_circle.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Terminal 1: PX4 SITL
+gnome-terminal -- bash -c "export DISPLAY=:0; sleep 2; cd ~/PX4-Autopilot && make px4_sitl gz_x500; exec bash"
+
+
+
+# Terminal 2: MicroXRCEAgent
+gnome-terminal -- bash -c "MicroXRCEAgent udp4 -p 8888; exec bash"
+
+# Terminal 3: QGroundControl
+gnome-terminal -- bash -c "~/QGroundControl-x86_64.AppImage; exec bash"
+
+# Terminal 4: ROS 2 Node
+gnome-terminal -- bash -c "cd ~/drone_circle && source install/setup.bash && source ./install/setup.bash && sleep 20s && ros2 run drone_circle_pkg drone_circle_node; exec bash"
+
