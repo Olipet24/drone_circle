e99258ef3fb26d5422ab93eb36110d764a78679c
Olipet24 <123438907+Olipet24@users.noreply.github.com>
2025-07-02 16:17:09 -0700

    Update launch_drone_circle.sh
    added comment to allow for singleHouseWorld world file to loaded in

--- launch_drone_circle.sh
+++ launch_drone_circle.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Terminal 1: PX4 SITL
-gnome-terminal -- bash -c "export DISPLAY=:0; sleep 2; cd ~/PX4-Autopilot && make px4_sitl gz_x500; exec bash"
-
+gnome-terminal -- bash -c "export DISPLAY=:0; sleep 2; cd ~/PX4-Autopilot && make px4_sitl gz_x500; exec bash" # comment this when loading the house world file
+# uncomment the below line to spawn the gazebo world file to 
+# gnome-terminal -- bash -c "export DISPLAY=:0; sleep 2; cd ~/PX4-Autopilot && PX4_GZ_WORLD=singleHouseWorld make px4_sitl gz_x500; exec bash"
 
 
 # Terminal 2: MicroXRCEAgent
